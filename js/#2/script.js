151ad5e8ca5726ab2c1166bc1f9d18ecae9a6f40
ErniBeCool <erni-96@mail.ru>
2023-02-21 20:22:43 +0600

    2nd app done

--- js/#2/script.js
+++ js/#2/script.js
@@ -19,7 +19,7 @@ let fontList = [
     "Cursive",
 ];
 
-const intializer = () => {
+const initializer = () => {
     highlighter(alignButtons, true);
     highlighter(spacingButtons, true);
     highlighter(formatButtons, false);
@@ -32,5 +32,65 @@ const intializer = () => {
         fontName.appendChild(option);
     })
 
-    
-}
+    for(let i = 1; i <= 7; i++){
+        let option = document.createElement("option");
+        option.value = i;
+        option.innerHTML = i;
+        fontSizeRef.appendChild(option);
+    }
+
+    fontSizeRef.value = 3;
+};
+
+const modifyText = (command, defaultUi, value) => {
+    document.execCommand(command, defaultUi, value);
+};
+
+optionsButtons.forEach((button) =>{
+    button.addEventListener("click", () =>{
+        modifyText(button.id, false, null);
+    });
+});
+
+advancedOptionButton.forEach((button) =>{
+    button.addEventListener("change", () =>{
+        modifyText(button.id, false, button.value);
+    });
+});
+
+linkButton.addEventListener("click", () => {
+    let userLink = prompt("Enter a URL?");
+    if (/http/i.test(userLink)) {
+        modifyText(linkButton.id, false, userLink);
+    } else {
+        userLink = "http://" + userLink;
+        modifyText(linkButton.id, false, userLink);
+        }
+});
+
+const highlighter = (className, needsRemoval) => {
+    className.forEach((button) => {
+        button.addEventListener("click", () =>{
+            if (needsRemoval) {
+                let alreadyActive = false;
+                if (button.classList.contains("active")) {
+                    alreadyActive = true;
+                }
+                highlighterRemover(className);
+                if(!alreadyActive) {
+                    button.classList.add("active");
+                }
+            } else {
+                button.classList.toggle("active");
+            }
+        });
+    });
+};
+
+const highlighterRemover = (className) => {
+    className.forEach((button) => {
+        button.classList.remove("active");
+    });
+};
+
+window.onload = initializer();
